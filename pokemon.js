b1ed0bac8dc53c1a9d1c17a0e26acfee3646640b
jarod4li <jared.rod23riguez@gmail.com>
2022-10-12 17:38:51 -0600

    added pic

--- pokemon.js
+++ pokemon.js
@@ -14,43 +14,46 @@ document.getElementById("pokemonButton").addEventListener("click", function(even
 //   data: Record<K, Omit<Query[K], '__typename'>>;
 // }
 
-fetch('https://graphqlpokemon.favware.tech/', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({
-    query: `
-      {
-        getPokemon(pokemon:`+lowvalue+`) {
-            num
-            species
-            color
-            shinySprite
-            smogonTier
-            smogonPage
-            serebiiPage
-            bulbapediaPage
-            abilities
-        }
-      }
-    `
-  })
-})
+// fetch('https://graphqlpokemon.favware.tech/', {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json'
+//   },
+//   body: JSON.stringify({
+//     query: `
+//       {
+//         getPokemon(pokemon:`+lowvalue+`) {
+//             num
+//             species
+//             color
+//             shinySprite
+//             smogonTier
+//             smogonPage
+//             serebiiPage
+//             bulbapediaPage
+//             abilities
+//         }
+//       }
+//     `
+//   })
+// })
+//   .then((res) => res.json())
+//   .then(function(json) {
+//     console.log(json);
+//     let results = "";
+//     results+= "<h2> This is " + value+ "</h2>";
+//     results+= "<p>Number:"+ json.data.getPokemon.num+"</p>";
+//     results+= "<img src='"+json.data.getPokemon.shinySprite+"'/>";
+//     results+= "<img src='"+json.data.getPokemon.smogonPage+"'/>";
+    
+//     document.getElementById("pokemonResults").innerHTML = results;
+//   });
+  fetch('https://api.pokemontcg.io/v2/cards') 
   .then((res) => res.json())
   .then(function(json) {
     console.log(json);
-    let results = "";
-    results+= "<h2> This is " + value+ "</h2>";
-    results+= "<p>Number:"+ json.data.getPokemon.num+"</p>";
-    results+= "<img src='"+json.data.getPokemon.shinySprite+"'/>";
-    results+= "<img src='"+json.data.getPokemon.smogonPage+"'/>";
-    
-    document.getElementById("pokemonResults").innerHTML = results;
-  });
-
   
-
+  });
 
 
   
