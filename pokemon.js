75a9cbab612d82bdbe9afbe67e60cc90930b749c
jarod4li <jared.rod23riguez@gmail.com>
2022-10-14 16:39:30 -0600

    ss

--- pokemon.js
+++ pokemon.js
@@ -1,13 +1,20 @@
 
 document.getElementById("pokemonButton").addEventListener("click", function(event) {
-    event.preventDefault();
-    
-    const value = document.getElementById("pokemonField").value;
-    let lowvalue = value.toLowerCase();
-    if (value === "")
-      return;
-    console.log(lowvalue);
-    
+  event.preventDefault();
+  
+  function capitalizeFirstLetter(str) {
+    return (str.charAt(0).toUpperCase() + str.slice(1));
+  }
+
+  const value = document.getElementById("pokemonField").value;
+  let lowvalue = value.toLowerCase();
+  let finalvalue = capitalizeFirstLetter(lowvalue);
+  if (value === "")
+    return;
+  console.log(lowvalue);
+  console.log(finalvalue);
+
+  
 // require ('@favware/graphql-pokemon');
 
 // interface GraphQLPokemonResponse<K extends keyof Omit<Query, '__typename'>> {
@@ -45,16 +52,23 @@ document.getElementById("pokemonButton").addEventListener("click", function(even
 //     results+= "<p>Number:"+ json.data.getPokemon.num+"</p>";
 //     results+= "<img src='"+json.data.getPokemon.shinySprite+"'/>";
 //     results+= "<img src='"+json.data.getPokemon.smogonPage+"'/>";
-    
+  
 //     document.getElementById("pokemonResults").innerHTML = results;
 //   });
-  fetch('https://api.pokemontcg.io/v2/cards') 
-  .then((res) => res.json())
-  .then(function(json) {
-    console.log(json);
-  
-  });
+fetch('https://api.pokemontcg.io/v2/cards?q=name:'+finalvalue) 
+.then((res) => res.json())
+.then(function(json) {
+  console.log(json);
+  let results = "";
+  console.log(json.data.find(obj => {
+    return obj.name === finalvalue
+  }));
+  results+= "<img class = \"card\" src = '"+json.data.find(obj => {
+    return obj.name === finalvalue
+  }).images.small+"'/>";
+  document.getElementById("pokemonResults").innerHTML = results;
+});
 
 
-  
-  });
+
+});
