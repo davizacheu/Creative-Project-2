22cc4c8ec54a0fc12445dcfa7c66f5477e982dfc
davizacheu <zacheudavi@gmail.com>
2022-10-15 23:43:51 -0600

    Davi's finished web

--- pokemon.js
+++ pokemon.js
@@ -1,7 +1,7 @@
 
 document.getElementById("pokemonButton").addEventListener("click", function(event) {
     event.preventDefault();
-    
+    document.getElementById("pokemonResults").style.opacity = 0;
     function capitalizeFirstLetter(str) {
       return (str.charAt(0).toUpperCase() + str.slice(1));
     }
@@ -14,47 +14,7 @@ document.getElementById("pokemonButton").addEventListener("click", function(even
     console.log(lowvalue);
     console.log(finalvalue);
 
-    
-// require ('@favware/graphql-pokemon');
-
-// interface GraphQLPokemonResponse<K extends keyof Omit<Query, '__typename'>> {
-//   data: Record<K, Omit<Query[K], '__typename'>>;
-// }
-
-// fetch('https://graphqlpokemon.favware.tech/', {
-//   method: 'POST',
-//   headers: {
-//     'Content-Type': 'application/json'
-//   },
-//   body: JSON.stringify({
-//     query: `
-//       {
-//         getPokemon(pokemon:`+lowvalue+`) {
-//             num
-//             species
-//             color
-//             shinySprite
-//             smogonTier
-//             smogonPage
-//             serebiiPage
-//             bulbapediaPage
-//             abilities
-//         }
-//       }
-//     `
-//   })
-// })
-//   .then((res) => res.json())
-//   .then(function(json) {
-//     console.log(json);
-//     let results = "";
-//     results+= "<h2> This is " + value+ "</h2>";
-//     results+= "<p>Number:"+ json.data.getPokemon.num+"</p>";
-//     results+= "<img src='"+json.data.getPokemon.shinySprite+"'/>";
-//     results+= "<img src='"+json.data.getPokemon.smogonPage+"'/>";
-    
-//     document.getElementById("pokemonResults").innerHTML = results;
-//   });
+  
   fetch('https://api.pokemontcg.io/v2/cards?q=name:'+finalvalue) 
   .then((res) => res.json())
   .then(function(json) {
@@ -65,8 +25,10 @@ document.getElementById("pokemonButton").addEventListener("click", function(even
     }));
     results+= "<img class = \"card\" src = '"+json.data.find(obj => {
       return obj.name === finalvalue
-    }).images.small+"'/>";
+    }).images.large+"'/>";
+    
     document.getElementById("pokemonResults").innerHTML = results;
+    document.getElementById("pokemonResults").style.opacity = 1;
   });
 
 
