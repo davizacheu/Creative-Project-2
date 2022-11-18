f123f5b41ff13ced3d4036b58ed1386607458f54
davizacheu <zacheudavi@gmail.com>
2022-11-17 18:38:46 -0700

    fading updated

--- pokemon.js
+++ pokemon.js
@@ -1,11 +1,14 @@
+let waitTime = 0;
 
 document.getElementById("pokemonButton").addEventListener("click", function(event) {
-    event.preventDefault();
-    document.getElementById("pokemonResults").style.opacity = 0;
-    function capitalizeFirstLetter(str) {
-      return (str.charAt(0).toUpperCase() + str.slice(1));
-    }
+  event.preventDefault();
+    
+  function capitalizeFirstLetter(str) {
+    return (str.charAt(0).toUpperCase() + str.slice(1));
+   }
 
+  document.getElementById("pokemonResults").style.opacity = 0;
+  setTimeout(() => {
     const value = document.getElementById("pokemonField").value;
     let lowvalue = value.toLowerCase();
     let finalvalue = capitalizeFirstLetter(lowvalue);
@@ -15,22 +18,24 @@ document.getElementById("pokemonButton").addEventListener("click", function(even
     console.log(finalvalue);
 
   
-  fetch('https://api.pokemontcg.io/v2/cards?q=name:'+finalvalue) 
-  .then((res) => res.json())
-  .then(function(json) {
-    console.log(json);
-    let results = "";
-    console.log(json.data.find(obj => {
-      return obj.name === finalvalue
-    }));
-    results+= "<img class = \"card\" src = '"+json.data.find(obj => {
-      return obj.name === finalvalue
-    }).images.large+"'/>";
+    fetch('https://api.pokemontcg.io/v2/cards?q=name:'+finalvalue) 
+    .then((res) => res.json())
+    .then(function(json) {
+      
+      console.log(json);
+      let results = "";
+      console.log(json.data.find(obj => {
+        return obj.name === finalvalue
+      }));
+      results+= "<img class = \"card\" src = '"+json.data.find(obj => {
+        return obj.name === finalvalue
+      }).images.large+"'/>";
+      
+      document.getElementById("pokemonResults").innerHTML = results;
+      document.getElementById("pokemonResults").style.opacity = 1;
+    });
+  }, waitTime);
     
-    document.getElementById("pokemonResults").innerHTML = results;
-    document.getElementById("pokemonResults").style.opacity = 1;
-  });
+  waitTime = 3000;
 
-
-  
   });
